bffe6301667cc62272d4548b63337d1eb96f3fb1
bniesobski <bartosz.niesobski@gmail.com>
2021-11-17 18:16:36 +0100

    home component updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // API
-import API from '../API'
+// import API from '../API'
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
+import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
+import Button from './Button';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -12,10 +18,35 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
 
-    const { state, loading, error } = useHomeFetch();
-    console.log(state); 
+    const { state, loading, error, searchTerm, setSerchTerm, setIsLoadingMore } = useHomeFetch();
+
+    if (error) return <div>Something went wrong...</div>
+
     return (
-        <div>Home page</div>
+        <>
+            {!searchTerm && state.results[1] &&
+                <HeroImage
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview} />
+            }
+            <SearchBar setSearchTerm={setSerchTerm} />
+            <Grid header={searchTerm ? 'Search result' : 'Popular movies'}>
+                {state.results.map(movie => (
+                    <Thumb
+                        key={movie.id}
+                        clickable
+                        image={movie.poster_path
+                            ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                            : NoImage}
+                        movieId={movie.id} />
+                ))}
+            </Grid>
+            {loading && <Spinner />}
+            {state.page < state.total_pages && !loading && (
+                <Button text='Load more' callback={() => setIsLoadingMore(prev => true)} />
+            )}
+        </>
     );
 }
 
